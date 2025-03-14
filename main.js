b12be5620904facaa7b5d6b39bed9da419bcbf6d
unknown <jolumici@gmail.com>
2025-03-14 17:33:20 -0300

    2a imprime

--- main.js
+++ main.js
@@ -14,8 +14,12 @@ for(i=0;i<3;i++){
     }
 }
 
-while(( jogadas<9 ) && ( fimJogo=false )){
-
+while(( jogadas<9 ) && ( fimJogo==false )){
+    console.log('   0   1   2 ')
+    for(i=0;i<3;i++){
+        console.log(`${i} [${tab[i][0]}] [${tab[i][1]}] [${tab[i][2]}]`);
+     }
+    
     lin=parseInt(prompt(`${jogador} em [linha]:`));
     col=parseInt(prompt(`${jogador} em [coluna]:`));
     
