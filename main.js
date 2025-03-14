0bbe26e8dfa2dae31df55cf35ef1e690c78e9147
unknown <jolumici@gmail.com>
2025-03-14 17:09:45 -0300

    criando-tab

--- main.js
+++ main.js
@@ -7,15 +7,24 @@ var vencedor=' ';
 var fimJogo = false;
 var tab = [3];
 
-while((jogadas<9)&&(fimJogo=false)){
+for(i=0;i<3;i++){
+    tab[i] = [3];
+    for(j=0;j<3;j++){
+        tab[i][j]=' ';
+    }
+}
+
+while(( jogadas<9 ) && ( fimJogo=false )){
 
     lin=parseInt(prompt(`${jogador} em [linha]:`));
     col=parseInt(prompt(`${jogador} em [coluna]:`));
 
 }
 
+
 if(vencedor==' '){
     console.log("empate\n");
 }else{
     console.log(`${vencedor} venceu`);
 }
+
